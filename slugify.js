52e4f7f03034193eec7132fd1975d888b147e073
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2021-01-29 18:08:28 -0600

    Improvements

--- slugify.js
+++ slugify.js
@@ -1,36 +1,47 @@
 require('isomorphic-fetch');
 const slug = require('slugg');
+const {default: PQueue} = require('p-queue');
 
 const tsProjectId = process.env.TS_PROJECT_ID;
 const tsApiKey = process.env.TS_API_KEY;
 const shape = process.env.TS_SHAPE;
 const origin = 'title';
 const target = 'slug';
+const batchSize = 10;
+const concurrency = 10;
 
 const endpoint = `https://api.takeshape.io/project/${tsProjectId}/graphql`
+const queue = new PQueue({concurrency});
+
+let from = 0;
 
 const slugify = async () => {
-  console.log('loading...');
+  console.log('loading more records...');
   const items = await getShapeList();
 
   if (items.length > 0) {
-    console.log('updating...');
-    const list = await updateShapes(items);
-    list.forEach(update => {
-      const result = update.result;
-      console.log(`-> ${origin}: ${result[origin]} -> ${target}: ${result[target]}`);
+    console.log('updating more records...');
+    queue.add(async () => {
+      const list = await updateShapes(items);
+      list.forEach(update => {
+        const result = update.result;
+        console.log(`-> ${origin}: ${result[origin]} -> ${target}: ${result[target]}`);
+      });
     });
-    
-    await slugify();
+    slugify();
   } else {
-    console.log('done...');
+    console.log('done loading...');
   }
 };
+
+queue.on('idle', () => {
+  console.log('done updating...');
+});
 
 const getShapeList = async () => {
   const data = await tsFetch(`
     query {
-      get${shape}List (where: {NOT: {${target}: {regexp: ".+"}}}, size: 10) {
+      get${shape}List (where: {NOT: {slug: {regexp: ".+"}}}, size: ${batchSize}, from: ${from},  sort: {field: "title", order: "asc"}) {
         items {
           _id
           ${origin}
@@ -40,10 +51,11 @@ const getShapeList = async () => {
   );
 
   if (!data) {
-    console.log('Could not get the Shape data. Make sure the Slug field exist.');
+    console.log(`Could not get the Shape data. Make sure the ${target} field exist.`);
     return;
   }
 
+  from += batchSize;
   const items = data[`get${shape}List`].items;
   return items;
 };
